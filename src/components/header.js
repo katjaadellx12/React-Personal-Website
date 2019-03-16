36281c757ba4cdfec9c0ad5d1253b5e8d98fb7a6
Pranav Ambwani <ambwani@usc.edu>
2019-03-16 02:13:08 -0700

    Test strip payments

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import './header.css'
+import StripeCheckout from 'react-stripe-checkout'
 
 class Header extends React.Component {
 
@@ -29,12 +30,38 @@ class Header extends React.Component {
     }
   }
 
+  handlePurchase = token => {
+    const amount = 50
+    const description = "My awesome services!"
+  
+    const bodyObject = {
+      tokenId: token.id,
+      email: token.email,
+      name: token.name,
+      description,
+      amount
+    }
+  
+    fetch('http://localhost:9000/stripe-payments', {
+      method: 'POST',
+      body: JSON.stringify(bodyObject)
+    })
+  }
+
   render() {
     return (
       <div className={ this.state.hasUserScrolled ? "Header HeaderScrolled": "Header" }>
         <div className="HeaderGroup">
           <Link to="/">Home</Link>
           <Link to="/blog">Blog</Link>
+          <StripeCheckout
+            amount={5000}
+            image="https://i.imgur.com/zI5yBin.jpg"
+            token={this.handlePurchase}  
+            stripeKey="pk_test_9s8hGtJ43DA8TkAwLyz7zHpw"
+          >
+              <button className={this.state.hasUserScrolled ? "Services ButtonOnScroll": "Services"}>Services</button>
+          </StripeCheckout>
         </div>
       </div>
     )
