08b575107bf5c3e970e39e7a163f5ce6cbce1fa6
Pranav Ambwani <ambwani@usc.edu>
2019-03-18 16:31:02 -0700

    Add google verification

--- src/components/seo.js
+++ src/components/seo.js
@@ -2,47 +2,48 @@ import React from 'react'
 import Helmet from 'react-helmet'
 
 function SEO({ description, lang, keywords, title, image }) {
-    return (
-        <Helmet
-            htmlAttributes={{
-                lang,
-            }}
-            
-            title={title}
-            
-            meta={[
-                {
-                name: `description`,
-                content: description,
-                },
-                {
-                name: `keywords`,
-                content: keywords,
-                },
-                {
-                property: `og:title`,
-                content: title,
-                },
-                {
-                property: `og:url`,
-                content: `https://pranav.tech`,
-                },
-                {
-                property: `og:image`,
-                content: image,
-                },
-                {
-                property: `og:description`,
-                content: description,
-                },
-                {
-                property: `og:type`,
-                content: `website`,
-                }
-            ]}
-        />
-    )
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      title={title}
+      meta={[
+        {
+          name: `google-site-verification`,
+          content: `yYiC-PlzGnCf6X0kj0ukm-uazDmWwu-GNdRHBhAMKqQ`,
+        },
+        {
+          name: `description`,
+          content: description,
+        },
+        {
+          name: `keywords`,
+          content: keywords,
+        },
+        {
+          property: `og:title`,
+          content: title,
+        },
+        {
+          property: `og:url`,
+          content: `https://pranav.tech`,
+        },
+        {
+          property: `og:image`,
+          content: image,
+        },
+        {
+          property: `og:description`,
+          content: description,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+      ]}
+    />
+  )
 }
 
 export default SEO
-
