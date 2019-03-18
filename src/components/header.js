dc9112615f3d3d86560e8f12d44d15b913c9998c
Pranav Ambwani <ambwani@usc.edu>
2019-03-18 16:40:16 -0700

    Remove Stripe checkout functionality

--- src/components/header.js
+++ src/components/header.js
@@ -2,19 +2,18 @@ import React from 'react'
 import Link from 'gatsby-link'
 import './header.css'
 import StripeCheckout from 'react-stripe-checkout'
-import ToggleDark from './toggle';
+import ToggleDark from './toggle'
 
 class Header extends React.Component {
-
   constructor(props) {
     super(props)
 
     this.state = {
-      hasUserScrolled: false
+      hasUserScrolled: false,
     }
   }
 
-  // As soon as the component loaded, listen to scroll events 
+  // As soon as the component loaded, listen to scroll events
   componentDidMount() {
     window.addEventListener('scroll', this.handleScroll)
   }
@@ -22,53 +21,54 @@ class Header extends React.Component {
   handleScroll = event => {
     const scrollTop = window.pageYOffset
 
-    if(scrollTop > 50) {
+    if (scrollTop > 50) {
       this.setState({ hasUserScrolled: true })
-    } 
-
-    else {
+    } else {
       this.setState({ hasUserScrolled: false })
     }
   }
 
   handlePurchase = token => {
     const amount = 50
-    const description = "My awesome services!"
-  
+    const description = 'My awesome services!'
+
     const bodyObject = {
       tokenId: token.id,
       email: token.email,
       name: token.name,
       description,
-      amount
+      amount,
     }
-  
+
     fetch('http://localhost:9000/stripe-payments', {
       method: 'POST',
-      body: JSON.stringify(bodyObject)
+      body: JSON.stringify(bodyObject),
     })
   }
 
   render() {
     return (
-      <div className={ this.state.hasUserScrolled ? "Header HeaderScrolled": "Header" }>
+      <div
+        className={
+          this.state.hasUserScrolled ? 'Header HeaderScrolled' : 'Header'
+        }
+      >
         <div className="HeaderGroup">
-          <ToggleDark/>
+          <ToggleDark />
           <Link to="/">Home</Link>
           <Link to="/blog">Blog</Link>
-          <StripeCheckout
+          {/* <StripeCheckout
             amount={5000}
             image="https://i.imgur.com/zI5yBin.jpg"
             token={this.handlePurchase}  
             stripeKey="pk_test_9s8hGtJ43DA8TkAwLyz7zHpw"
           >
               <button className={this.state.hasUserScrolled ? "Services ButtonOnScroll": "Services"}>Services</button>
-          </StripeCheckout>
+          </StripeCheckout> */}
         </div>
       </div>
     )
   }
 }
-
 
 export default Header
