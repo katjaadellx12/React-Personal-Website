4be586270152c1a9664160e82847ce8ed855f980
Pranav Ambwani <ambwani@usc.edu>
2019-03-18 19:03:53 -0700

    Configure robots.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,23 @@
 module.exports = {
   siteMetadata: {
     title: 'Pranav Ambwani',
-    description: 'Welcome to the website that I coded from scratch leveraging several frameworks like React to ensure a simple, yet elegant design.',
+    description:
+      'Welcome to the website that I coded from scratch leveraging several frameworks like React to ensure a simple, yet elegant design.',
     url: 'https://pranav.tech', // No trailing slash allowed!
-    keywords: 'pranav, ambwani, pranav ambwani, Pranav Ambwani, Pranav, Ambwani, LG Electronics, LavaLab, LavaLab USC, USC, American Express, IBM, Product Management, Program Management, Technical Product Management, Product, product, management, software developer, software, developer, javascript, c++, University of Southern California',
-    image: 'icon.jpg'
+    keywords:
+      'pranav, ambwani, pranav ambwani, Pranav Ambwani, Pranav, Ambwani, LG Electronics, LavaLab, LavaLab USC, USC, American Express, IBM, Product Management, Program Management, Technical Product Management, Product, product, management, software developer, software, developer, javascript, c++, University of Southern California',
+    image: 'icon.jpg',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-dark-mode'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-dark-mode',
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://pranav.tech',
+        sitemap: 'https://pranav.tech/sitemap.xml',
+        policy: [{ userAgent: '*', allow: '/' }],
+      },
+    },
+  ],
 }
