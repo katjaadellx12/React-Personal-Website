fe10d468d295ff6d828c34d2774923b71cc78c57
Pranav Ambwani <ambwani@usc.edu>
2019-03-18 21:24:43 -0700

    Use sitemap v1 since v2 breaks code

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     description:
       'Welcome to the website that I coded from scratch leveraging several frameworks like React to ensure a simple, yet elegant design.',
     url: 'https://pranav.tech', // No trailing slash allowed!
+    siteUrl: 'https://pranav.tech',
     keywords:
       'pranav, ambwani, pranav ambwani, Pranav Ambwani, Pranav, Ambwani, LG Electronics, LavaLab, LavaLab USC, USC, American Express, IBM, Product Management, Program Management, Technical Product Management, Product, product, management, software developer, software, developer, javascript, c++, University of Southern California',
     image: 'icon.jpg',
@@ -11,6 +12,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-dark-mode',
+    'gatsby-plugin-sitemap',
     {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
