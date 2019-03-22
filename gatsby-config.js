3f7e827951718e8b427a9264dc8ec5361789fe59
Pranav Ambwani <ambwani@usc.edu>
2019-03-22 16:59:48 -0700

    Custom sitemap for gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,13 +12,34 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-dark-mode',
-    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-sitemap',
+      options: {
+        serialize: ({ site, allSitePage }) =>
+          allSitePage.edges.map(edge => {
+            let path = edge.node.path
+            let priority = 0.5
+            if (path === '/') {
+              priority = 1.0
+            } else if (path.match(/blog/)) {
+              priority = 0.6
+            } else {
+              priority = 0.2
+            }
+            console.log('priority: ', priority.toFixed(1), '    path: ', path)
+            return {
+              url: site.siteMetadata.siteUrl + edge.node.path,
+              changefreq: `monthly`,
+              priority,
+            }
+          }),
+      },
+    },
     {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
         host: 'https://pranav.tech',
         sitemap: 'https://pranav.tech/sitemap.xml',
-        policy: [{ userAgent: '*', allow: '/' }],
       },
     },
     {
