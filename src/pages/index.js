576b05eb195e7842534f50dfa06a3fa5b3abf874
Pranav Ambwani <ambwani@usc.edu>
2019-03-23 02:37:01 -0700

    Audit

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
   <div>
     <SEO
       title="Pranav | Home"
-      description="Check out the website that I coded from scratch leveraging several frameworks to ensure a simple, yet elegant design."
+      description="A blazing fast website that I coded from scratch leveraging several frameworks like React to ensure a simple, yet elegant design."
       keywords="pranav, ambwani, pranav ambwani, Pranav Ambwani, Pranav, Ambwani, LG Electronics, LavaLab,USC, American Express, IBM, Product Management, Program Management, Technical Product Management, Product, product, management, software developer, software, developer, javascript, c++"
       image="https://i.imgur.com/A29myzy.png"
     />
