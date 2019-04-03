bdca200ce904905ec81f393280a34e403fe6cc54
Pranav Ambwani <ambwani@usc.edu>
2019-04-03 02:02:30 -0700

    Media -- mobile first

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import resumeSvg from '../images/Resume.svg'
 import SEO from '../components/seo'
+import Wave from '../components/wave'
 
 const Resume = () => (
   <ResumeContainer>
@@ -17,7 +18,38 @@ const ResumeContainer = styled.div`
   justify-content: center;
   margin-top: 50px;
 
-  @media (max-width: 680px) {
+  @media only screen and (max-width: 400px) {
+    transform: scale(0.4);
+    margin-top: -100px;
+  }
+
+  /* Extra small devices (phones, 600px and down) */
+  @media only screen and (max-width: 600px) {
+    transform: scale(0.5);
+    margin-top: -100px;
+  }
+
+  /* Small devices (portrait tablets and large phones, 600px and up) */
+  @media only screen and (min-width: 600px) {
+    transform: scale(0.7);
+  }
+
+  /* Medium devices (landscape tablets, 768px and up) */
+  @media only screen and (min-width: 768px) {
+    transform: scale(0.85);
+  }
+
+  /* Large devices (laptops/desktops, 992px and up) */
+  @media only screen and (min-width: 992px) {
+    transform: scale(0.9);
+  }
+
+  /* Extra large devices (large laptops and desktops, 1200px and up) */
+  @media only screen and (min-width: 1200px) {
+    transform: scale(1);
+  }
+
+  /* @media (max-width: 680px) {
     margin-top: 20px;
     transform: scale(0.9);
   }
@@ -25,5 +57,5 @@ const ResumeContainer = styled.div`
   @media (max-width: 600px) {
     margin-top: -150px;
     transform: scale(0.6);
-  }
+  } */
 `
