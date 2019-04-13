e22b8670f1145068d6ec76b8d90b147c3d027d4a
Pranav Ambwani <ambwani@usc.edu>
2019-04-13 04:00:26 -0700

    Configure google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,21 +49,14 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-gtag`,
       options: {
-        trackingId: 'UA-101265890-2',
-        head: true,
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-gtag`,
-    //   options: {
-    //     trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
-    //     // Puts tracking script in the head instead of the body
-    //     head: false,
-    //     // enable ip anonymization
-    //     anonymize: true,
-    //   },
-    // },
   ],
 }
